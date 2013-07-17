375806329cf89e222ea318dbe58ed761ac8cc392
Daniel Herrero <daniel.herrero@kaleidos.net>
2013-07-17 15:31:27 +0200

    US13377 #13420 New tests for search service

--- test/integration/net/kaleidos/komunumo/SearchServiceIntegrationSpec.groovy
+++ test/integration/net/kaleidos/komunumo/SearchServiceIntegrationSpec.groovy
@@ -115,4 +115,87 @@ class SearchServiceIntegrationSpec extends IntegrationSpec {
 			activitiesNextWeek.size() == 3
 			activitiesNextMonth.size() == 4
 	}
+	
+	void "searching activities using all the whole search fields "() {
+		setup:
+		
+			def city = City.build()
+			def entity = Entity.build(name: 'Entity1')
+			
+			def neighbourhood1 = Neighbourhood.build(city: city, name: 'neighbourhood1')
+			def neighbourhood2 = Neighbourhood.build(city: city, name: 'neighbourhood2')
+			def neighbourhood3 = Neighbourhood.build(city: city, name: 'neighbourhood3')
+			
+			def activityType1 = ActivityType.build(name: 'activityType1')
+			def activityType2 = ActivityType.build(name: 'activityType2')
+			
+			def activity0, activity1, activity2, activity3
+			def now, restOfDay, tomorrow, nextThreeDays, nextWeek,nextMonth
+			def realNow = new Date()
+			
+			use(TimeCategory) {
+				now = new Date().clearTime()
+				restOfDay = now + 1.days
+				tomorrow = now + 2.days
+				nextThreeDays = now + 4.days
+				nextWeek = now + 1.week + 1.day
+				nextMonth = now + 1.months + 1.day
+
+				activity0 = Activity.build(activityDate: realNow + 5.minutes, neighbourhood: neighbourhood2, activityType: activityType2, entity: entity, name: 'Activity0')
+				activity1 = Activity.build(activityDate: tomorrow - 5.minutes, neighbourhood: neighbourhood1, activityType: activityType1, entity: entity, name: 'Activity1')
+				activity2 = Activity.build(activityDate: nextThreeDays - 5.minutes, neighbourhood: neighbourhood2, activityType: activityType1, entity: entity, name: 'Activity2')
+				activity3 = Activity.build(activityDate: nextWeek + 5.hours, neighbourhood: neighbourhood2, activityType: activityType2, entity: entity, name: 'Activity3')
+			}
+			
+		when:
+			def activitiesNextWeek1 = searchService.activitySearch(neighbourhood2, activityType2, now, nextWeek)
+			def activitiesNextWeek2 = searchService.activitySearch(neighbourhood2, activityType1, now, nextWeek)
+
+			
+		then:
+			activitiesNextWeek1.size() == 1
+			activitiesNextWeek1[0] == activity0
+			
+			activitiesNextWeek2.size() == 1
+			activitiesNextWeek2[0] == activity2
+	}
+	
+	
+	void "searching activities using none search fields "() {
+		setup:
+		
+			def city = City.build()
+			def entity = Entity.build(name: 'Entity1')
+			
+			def neighbourhood1 = Neighbourhood.build(city: city, name: 'neighbourhood1')
+			def neighbourhood2 = Neighbourhood.build(city: city, name: 'neighbourhood2')
+			def neighbourhood3 = Neighbourhood.build(city: city, name: 'neighbourhood3')
+			
+			def activityType1 = ActivityType.build(name: 'activityType1')
+			def activityType2 = ActivityType.build(name: 'activityType2')
+			
+			def activity0, activity1, activity2, activity3
+			def now, restOfDay, tomorrow, nextThreeDays, nextWeek,nextMonth
+			def realNow = new Date()
+			
+			use(TimeCategory) {
+				now = new Date().clearTime()
+				restOfDay = now + 1.days
+				tomorrow = now + 2.days
+				nextThreeDays = now + 4.days
+				nextWeek = now + 1.week + 1.day
+				nextMonth = now + 1.months + 1.day
+
+				activity0 = Activity.build(activityDate: realNow + 5.minutes, neighbourhood: neighbourhood2, activityType: activityType2, entity: entity, name: 'Activity0')
+				activity1 = Activity.build(activityDate: tomorrow - 5.minutes, neighbourhood: neighbourhood1, activityType: activityType1, entity: entity, name: 'Activity1')
+				activity2 = Activity.build(activityDate: nextThreeDays - 5.minutes, neighbourhood: neighbourhood2, activityType: activityType1, entity: entity, name: 'Activity2')
+				activity3 = Activity.build(activityDate: nextWeek + 5.hours, neighbourhood: neighbourhood2, activityType: activityType2, entity: entity, name: 'Activity3')
+			}
+			
+		when:
+			def allActivities = searchService.activitySearch(null, null, null, null)
+			
+		then:
+			allActivities.size() == 4
+	}
 }
