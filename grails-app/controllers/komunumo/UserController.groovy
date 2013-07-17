ed3e1cdc2bd526438428ce726ad2a94a74b5098d
Daniel Herrero <daniel.herrero@kaleidos.net>
2013-07-17 09:45:40 +0200

    User controller secured

--- grails-app/controllers/komunumo/UserController.groovy
+++ grails-app/controllers/komunumo/UserController.groovy
@@ -1,8 +1,9 @@
 package komunumo
 
 import org.springframework.dao.DataIntegrityViolationException
+import grails.plugins.springsecurity.Secured
 
-
+@Secured("isAuthenticated()")
 class UserController {
 	static scaffold = true
 }
