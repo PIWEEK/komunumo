dd908095f2a89e44ce448694ed4a5fbcc2490e83
Daniel Herrero <daniel.herrero@kaleidos.net>
2013-07-18 13:09:33 +0200

    US13378 #13435 Sending parameters to view

--- grails-app/controllers/komunumo/IndexController.groovy
+++ grails-app/controllers/komunumo/IndexController.groovy
@@ -1,10 +1,12 @@
 package komunumo
 
+import net.kaleidos.komunumo.ActivityType;
+import net.kaleidos.komunumo.Neighbourhood;
 import grails.plugins.springsecurity.Secured
 
 class IndexController {
 
     def show() {
-		 render view:'/index', model:[dataView: 'HomeView']
+		 render view:'/index', model:[dataView: 'HomeView', neigbourhoodList: Neighbourhood.list(), activityList: ActivityType.list()]
 	}
 }
