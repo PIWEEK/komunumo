87dc05f1fd80ec2dc2e63148dbb21b805825f5e2
Teresa de la Torre <teresa.delatorre@kaleidos.net>
2013-07-18 13:30:28 +0200

    Add controller to activityMap

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,17 +1,19 @@
 class UrlMappings {
 
 	static mappings = {
-		
+
 		"/$controller/$action?/$id?"{
 			constraints {
 				// apply constraints here
 			}
 		}
-		
+
 		name admin: "/admin" {controller = 'admin'; action = 'scaffolding'}
 		name search: "/api/activity/search" {controller = 'search'; action = 'activitySearch'}
+		name activityMap: "/api/activity/map" {controller = 'activity'; action = 'activityMap'}
         name index: "/" {controller = 'index'; action = 'show'}
-		
+        name activityShow: "/activity/show" {controller = 'activity'; action = 'activityShow'}
+
 		"/login/$action?"(controller: "login")
 		"/logout/$action?"(controller: "logout")
 
--- grails-app/controllers/net/kaleidos/komunumo/ActivityController.groovy
+++ grails-app/controllers/net/kaleidos/komunumo/ActivityController.groovy
@@ -2,8 +2,63 @@ package net.kaleidos.komunumo
 
 import org.springframework.dao.DataIntegrityViolationException
 import grails.plugins.springsecurity.Secured
+import groovy.time.TimeCategory
 
 @Secured("isAuthenticated()")
 class ActivityController {
-	static scaffold = true	
+	static scaffold = true
+
+    def activityMap(String neighbourhoodId, String activityTypeId, String activityDateOption) {
+        if (neighbourhoodId) {
+            neighbourhood = Neighbourhood.get(neighbourhoodId)
+            if (!neighbourhood) {
+				neighbourhood = null
+			}
+        }
+
+        if (activityTypeId) {
+            activityType = ActivityType.get(activityTypeId)
+            if (!activityType) {
+                activityType = null
+            }
+
+        if (activityDateOption) {
+            use(TimeCategory) {
+				switch (activityDateOption) {
+				case 0:
+					nextDate = now + 1.days
+					break;
+				case 1:
+					nextDate = now + 2.days
+					break;
+				case 3:
+					nextDate = now + 4.days
+					break;
+				case 7:
+					nextDate = now + 1.week + 1.day
+					break;
+				case 30:
+					nextDate = now + 1.months + 1.day
+					break;
+				}
+			}
+        }
+
+        //~ def activitiesList = searchService.activitySearch(neighbourhood, activityType, now, nextDate)
+        def activitiesList = Activity.list()
+
+        def text = 'lat\tlon\ticon\ticonSize\ticonOffset\ttitle\tdescription\tpopupSize\n'
+
+        activitiesList.each { activity ->
+            if (activity.latitude && activity.longitude) {
+                def domain = "localhost:8080"
+                def title = "<a href=" + "\"" + "http://" + domain + "/activity/show/" + activity.id + "\">" + activity.name + "</a>"
+                def description = "<br/>" + activity.description
+
+                text += activity.latitude + "\t" + activity.longitude + "\t" + "/images/marker.png" + "\t" + 32 + "," + 32 + "\t" + -16 + "," + -16 + "\t" + title + "\t" + description + "\t" + 300 + "," + 120 + "\n"
+            }
+        }
+
+        //~ return HttpResponse(text, content_type='text/plain')
+    }
 }
