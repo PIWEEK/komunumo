b60980804efe542fc75379b58f598911b4b11143
Teresa de la Torre <teresa.delatorre@kaleidos.net>
2013-07-19 11:24:53 +0200

    Delete println

--- grails-app/controllers/net/kaleidos/komunumo/SearchController.groovy
+++ grails-app/controllers/net/kaleidos/komunumo/SearchController.groovy
@@ -6,21 +6,21 @@ import groovy.time.TimeCategory
 
 
 class SearchController {
-	
+
 	def searchService
 
     def activitySearch(String neighId, String aTypeId, String nextDate) {
-		
+
 		def success = true
-		
+
 		def neighbourhood = null
 		if (neighId) {
 			neighbourhood = Neighbourhood.get(neighId)
 			if (!neighbourhood) {
 				success = false
-			} 
+			}
 		}
-		
+
 		def activityType = null
 		if (aTypeId) {
 			activityType = ActivityType.get(aTypeId)
@@ -28,24 +28,24 @@ class SearchController {
 				success = false
 			}
 		}
-		
+
 		def endDate, realNow, now
 
 		if (nextDate) {
-			
+
 			realNow = new Date()
 			now = realNow.clearTime()
-			
-			use(TimeCategory) {				
+
+			use(TimeCategory) {
 				switch (nextDate) {
 				case "0":
 					endDate = now + 1.days
 					break;
 				case "1":
-					endDate = now + 2.days		
-					break;	
+					endDate = now + 2.days
+					break;
 				case "3":
-					endDate = now + 4.days		
+					endDate = now + 4.days
 					break;
 				case "7":
 					endDate = now + 1.week + 1.day
@@ -60,10 +60,9 @@ class SearchController {
 				}
 			}
 		}
-		
+
 		def result = searchService.activitySearch(neighbourhood, activityType, realNow, endDate)
 		if (success) {
-println "====================>  + ${result}"			
 			return render(text:[success:success, list:searchService.activityJsonFormatter(result)] as JSON, contentType:'text/json')
 		} else {
 		return render(text:[success:success, list:[]] as JSON, contentType:'text/json')
